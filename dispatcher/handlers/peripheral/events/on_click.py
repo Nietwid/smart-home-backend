e7a65730e9502f5ebc534925996c7e6c9ea30e03
Dawid Franczak <daw.franczak@gmail.com>
2026-04-03 15:31:34 +0200

    Add measurement event handlers

--- dispatcher/handlers/peripheral/events/on_click.py
+++ dispatcher/handlers/peripheral/events/on_click.py
@@ -1,8 +1,5 @@
-from dispatcher.command_message.message import CommandMessage
 from dispatcher.device.messages.enum import MessageCommand
-from device.models import Device
-from dispatcher.handlers.base import ActionEventBaseHandler, EventIntentBaseHandler
-from dispatcher.dispatch_result import DispatchResult
+from dispatcher.handlers.base import EventIntentBaseHandler
 from dispatcher.device.messages.enum import Scope, MessageType, MessageDirection
 from dispatcher.handlers.registry import register_action_event
 from notifier.enum import MicroserviceQueueName
