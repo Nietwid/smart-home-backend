d8737bce234a550a6ae5020f9b3581fa1ebb6e17
Dawid Franczak <daw.franczak@gmail.com>
2026-04-29 20:51:00 +0200

    add on blink state manipulate

--- dispatcher/handlers/peripheral/events/on_blink.py
+++ dispatcher/handlers/peripheral/events/on_blink.py
@@ -1,6 +1,4 @@
-from device.models import Device
 from dispatcher.device.messages.enum import MessageCommand
-from dispatcher.command_message.message import CommandMessage
 from dispatcher.device.messages.payload.lamp import OnBlinkPayload, OnBlinkStatus
 from dispatcher.handlers.base import EventIntentBaseHandler
 from dispatcher.device.messages.enum import (
@@ -10,10 +8,7 @@ from dispatcher.device.messages.enum import (
 )
 from dispatcher.handlers.registry import register_action_event
 from notifier.enum import RabbitExchange, RabbitRoutingKey
-from notifier.factory.frontend_notifier_factory import frontend_notifier_factory
-from notifier.message import NotifierMessage
 from peripherals.models import Peripherals
-from redis_cache import redis_cache
 
 
 @register_action_event(
@@ -26,28 +21,10 @@ class OnBlinkEventIntentHandler(EventIntentBaseHandler):
     exchange = RabbitExchange.SENSOR_SERVICE
     routing_key = RabbitRoutingKey.EVENTS
 
-    def process_message(self, message: CommandMessage) -> None:
-        peripheral: Peripherals = message.peripheral
-        payload: OnBlinkPayload = message.payload
+    def update_peripheral_state(self, peripheral: Peripherals, payload: OnBlinkPayload):
         if payload.status == OnBlinkStatus.START:
-            redis_cache.add_peripheral_pending(
-                peripheral.pk, message.command, timeout=90
-            )
+            peripheral.state["is_on"] = True
+            peripheral.save(update_fields=["state"])
         elif payload.status == OnBlinkStatus.STOP:
-            redis_cache.delete_peripheral_pending(peripheral.pk, message.command)
-        else:
-            raise ValueError(f"Invalid status: {payload.status}")
-
-    def get_extra_notification(self, message: CommandMessage) -> list[NotifierMessage]:
-        device: Device = message.device
-        peripheral: Peripherals = message.peripheral
-        home_id = device.home.pk
-        pending = redis_cache.get_peripherals_pending(peripheral.pk)
-        return [
-            frontend_notifier_factory.update_peripheral_pending(
-                home_id=home_id,
-                pending=pending,
-                device_id=device.pk,
-                peripheral_id=peripheral.pk,
-            ),
-        ]
+            peripheral.state["is_on"] = False
+            peripheral.save(update_fields=["state"])
